2c3824af0d95d5691d39d19616b4c3a68e48c7b4
Pingjun <chenpingjun1990@ufl.edu>
2016-09-26 22:58:47 -0400

    add Flask demo

---
+++ HelloFlask.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask import request, redirect, url_for, abort
+import os, sys
+
+app = Flask(__name__)
+
+# Setting global variables
+app.config['UPLOAD_FOLDER'] = 'static/files/upload_data/'
+if not os.path.isdir(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+
+@app.route('/')
+def hello_flask():
+   return 'Hello Flask!'
+
+@app.route('/hello/<name>', methods=['POST', 'GET'])
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+if __name__ == '__main__':
+    app.debug = True
+    # app.run(host='0.0.0.0', port=32121)
+    app.run(host='0.0.0.0')
